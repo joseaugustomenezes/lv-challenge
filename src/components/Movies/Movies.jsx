618a81f38fea49ff390e6b41f675a9d69e9b890f
Jose Menezes <jaugustonmn@gmail.com>
2024-07-07 19:34:24 -0300

    create useInfiniteScroll.js and use it inside Movies.jsx

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,73 +1,22 @@
 import Movie from '../Movie';
 import '../../styles/movies.scss';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import './Movies.css';
-import { useEffect, useRef } from 'react';
-import { fetchMovies } from '../../data/moviesSlice';
-import { ENDPOINT_DISCOVER, ENDPOINT_SEARCH } from '../../constants';
+import { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import useInfiniteScroll from './useInfiniteScroll';
 
 const Movies = () => {
-  const {
-    movies,
-    fetchStatus,
-    lastFetchType,
-    lastFetchPage,
-    totalPages,
-    movieIds,
-  } = useSelector((state) => state.movies);
-  const loader = useRef(null);
-  const dispatch = useDispatch();
+  const { movies, fetchStatus, movieIds } = useSelector(
+    (state) => state.movies,
+  );
   const [searchParams] = useSearchParams();
   const searchQuery = searchParams.get('search') || '';
+  const { loader } = useInfiniteScroll();
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'instant' });
   }, [searchQuery]);
-
-  useEffect(() => {
-    let loaderValue;
-    const params = new URLSearchParams(window.location.search);
-    const searchQuery = params.get('search');
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting && lastFetchPage < totalPages) {
-          if (lastFetchType === 'search') {
-            dispatch(
-              fetchMovies({
-                apiUrl: `${ENDPOINT_SEARCH}${searchQuery}&page=${lastFetchPage + 1}`,
-                fetchType: 'search',
-                searchedTerm: searchQuery,
-              }),
-            );
-          } else {
-            dispatch(
-              fetchMovies({
-                apiUrl: `${ENDPOINT_DISCOVER}&page=${lastFetchPage + 1}`,
-                fetchType: 'discover',
-              }),
-            );
-          }
-        }
-      },
-      {
-        root: null,
-        rootMargin: '20px',
-      },
-    );
-
-    if (loader.current) {
-      observer.observe(loader.current);
-      loaderValue = loader.current;
-    }
-
-    return () => {
-      if (loaderValue) {
-        observer.unobserve(loaderValue);
-      }
-    };
-  }, [lastFetchType, movies, lastFetchPage, totalPages, dispatch]);
 
   if (fetchStatus === 'error') {
     return <TextMessage message="Error fetching movies." />;
